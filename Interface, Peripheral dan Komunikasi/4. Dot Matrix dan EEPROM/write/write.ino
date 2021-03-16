4e5d9f24470633607bff356c25f747ac0534029e
itsfathan <79251816+itsfathan@users.noreply.github.com>
2021-03-16 22:38:08 +0700

    Update write.ino

--- Interface, Peripheral dan Komunikasi/4. Dot Matrix dan EEPROM/write/write.ino
+++ Interface, Peripheral dan Komunikasi/4. Dot Matrix dan EEPROM/write/write.ino
@@ -1,18 +1,18 @@
-#include <EEPROM.h> 
-int addr = 0; 
-int potensio = A0; 
+#include <EEPROM.h> // menambahkan modul EEPROM.h
+int addr = 0; //inisialisasikan int addr = 0 
+int potensio = A0; // menjelaskan bahwa potesio adalah pin A0
 void setup() 
 { 
-Serial.begin(9600); 
+Serial.begin(9600); // akan ditampilkan ke serial monitor
 } 
 void loop() { 
- int val = analogRead(potensio) / 4; 
-if (addr <= 512) {
-EEPROM.write(addr, val); 
-Serial.print(val); 
-Serial.print("\t"); 
-Serial.println(addr); 
-addr = addr + 1; 
+ int val = analogRead(potensio) / 4; // potensio akan dibaca sebagai anolog
+if (addr <= 512) {// jika nilai addr kurang dari 512
+EEPROM.write(addr, val); // maka EEPROM menuliskan isi dari addr dan val
+Serial.print(val); // akan menampilkan isi val ke serial monitor
+Serial.print("\t"); // menambahkan spasi
+Serial.println(addr); // akan menampilkan isi dari addr
+addr = addr + 1; // addr di tambah 1 jad addr bernilai 1
 } 
 delay(100); 
 }
