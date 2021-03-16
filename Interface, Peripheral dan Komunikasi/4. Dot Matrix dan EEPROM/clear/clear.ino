e14ef6815043f941f1b40fb3aa25ae0d99b93094
itsfathan <79251816+itsfathan@users.noreply.github.com>
2021-03-16 22:32:17 +0700

    Update clear.ino

--- Interface, Peripheral dan Komunikasi/4. Dot Matrix dan EEPROM/clear/clear.ino
+++ Interface, Peripheral dan Komunikasi/4. Dot Matrix dan EEPROM/clear/clear.ino
@@ -1,8 +1,9 @@
 #include <EEPROM.h> 
 void setup() 
 { 
- for (int i = 0; i < 512; i++) 
-EEPROM.write(i, 0); digitalWrite(13, 
+ for (int i = 0; i < 512; i++) // perulangan penulisan EEPROM
+EEPROM.write(i, 0); // EEPROM akan menjadi 0
+digitalWrite(13, // pin 13 akan HIGH
 HIGH); 
 } 
 void loop() 
