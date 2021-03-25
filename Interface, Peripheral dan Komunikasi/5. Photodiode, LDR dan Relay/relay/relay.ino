865d62cf97644b73167261a1964a266cefae22a7
rahmanwghazi <rahmanwghazi@student.telkomuniversity.ac.id>
2021-03-25 12:43:15 +0700

    update ulang

--- Interface, Peripheral dan Komunikasi/5. Photodiode, LDR dan Relay/relay/relay.ino
+++ Interface, Peripheral dan Komunikasi/5. Photodiode, LDR dan Relay/relay/relay.ino
@@ -1,10 +1,10 @@
-int relay = A5; 
+int relay = A5; // men set pin A5 sebagai relay
 void setup() { 
-pinMode(relay, OUTPUT); 
+pinMode(relay, OUTPUT); // menset relay sebagai output
 } 
 void loop() { 
- digitalWrite(relay, HIGH); 
-delay(1000); 
-digitalWrite(relay, LOW); 
-delay(1000); 
+ digitalWrite(relay, HIGH); //menyalakan relay keadaan HIGH
+delay(1000); // memberi delay selama 1 detik
+digitalWrite(relay, LOW); //menyalakan relay keadaan LOW
+delay(1000); // memberi delay selama 1 detik
 } 
