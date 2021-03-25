d793ef6f3547978aa94a160496b66f0a0c45e829
itsfathan <79251816+itsfathan@users.noreply.github.com>
2021-03-25 11:58:38 +0700

    Update ldr.ino

--- Interface, Peripheral dan Komunikasi/5. Photodiode, LDR dan Relay/ldr/ldr.ino
+++ Interface, Peripheral dan Komunikasi/5. Photodiode, LDR dan Relay/ldr/ldr.ino
@@ -1,15 +1,15 @@
-int lightPin = A0; 
-int ledPin = 13; 
+int lightPin = A0; // untuk set pin A0
+int ledPin = 13; // set pin 13 unuk LED
 void setup() 
 { 
  Serial.begin(9600); 
- pinMode( ledPin, OUTPUT ); 
+ pinMode( ledPin, OUTPUT ); // menset pin ledpin sebagai output
 } 
 void loop() 
 { 
-Serial.println(analogRead(lightPin)); 
-analogWrite(ledPin, analogRead(lightPin)/4); 
-delay(10); 
+Serial.println(analogRead(lightPin)); // memprintt ke serial monitor hasil dari analogread lightpin
+analogWrite(ledPin, analogRead(lightPin)/4); // menuliskan ke ouput dengan hasil  yang di bagi 4
+delay(10); // delay 10ms
 } 
 
 //C:\\Users\\mrwgj\\AppData\\Local\\Temp\\arduino_build_91725/ldr.ino.hex
