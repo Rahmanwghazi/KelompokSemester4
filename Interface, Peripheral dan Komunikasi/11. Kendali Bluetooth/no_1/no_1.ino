6c0dedfa52400f0a0e00feb70b9c7d70a15c09d3
rahmanwghazi <rahmanwghazi@student.telkomuniversity.ac.id>
2021-05-22 13:16:27 +0700

    modul 11 bluetooth

--- Interface, Peripheral dan Komunikasi/11. Kendali Bluetooth/no_1/no_1.ino
+++ Interface, Peripheral dan Komunikasi/11. Kendali Bluetooth/no_1/no_1.ino
@@ -1,26 +1,28 @@
-String control; 
-void setup() {
-pinMode(13, OUTPUT); 
-Serial.begin(9600);
-} 
-void loop() {
- while (Serial.available())
- { 
- delay(10); 
- char c = Serial.read(); 
- control += c; 
- } 
- if (control.length() > 0)
- {
- Serial.println(control); 
- if(control == "on")
- {
- digitalWrite(13, HIGH);
- } 
- if(control == "off")
- {
- digitalWrite(13, LOW);
- } 
-control="";
+String control;
+void setup()
+{
+    pinMode(13, OUTPUT);
+    Serial.begin(9600);
 }
+void loop()
+{
+    while (Serial.available())
+    {
+        delay(10);
+        char c = Serial.read();
+        control += c;
+    }
+    if (control.length() > 0)
+    {
+        Serial.println(control);
+        if (control == "on")
+        {
+            digitalWrite(13, HIGH);
+        }
+        if (control == "off")
+        {
+            digitalWrite(13, LOW);
+        }
+        control = "";
+    }
 }
