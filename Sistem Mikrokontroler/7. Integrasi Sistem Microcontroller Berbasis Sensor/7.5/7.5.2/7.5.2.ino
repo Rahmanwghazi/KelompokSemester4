849999a30dc06ab862d64e1c4dda609c6dcc60cf
rahmanwghazi <rahmanwghazi@student.telkomuniversity.ac.id>
2021-05-26 09:03:44 +0700

    mikro 7 bagian 7.5

--- Sistem Mikrokontroler/7. Integrasi Sistem Microcontroller Berbasis Sensor/7.5/7.5.2/7.5.2.ino
+++ Sistem Mikrokontroler/7. Integrasi Sistem Microcontroller Berbasis Sensor/7.5/7.5.2/7.5.2.ino
@@ -1,12 +1,12 @@
-int lm35 = A2;
+int lm = A2;
 int led = A1;
 
 void setup(){
-  pinMode(lm35,INPUT);
+  pinMode(lm,INPUT);
   pinMode(led, OUTPUT);
 }
 void loop(){
-  double suhu = analogRead(lm35)/2-1;
+  double suhu = analogRead(lm)/2-1;
 
   if(suhu>=38.5){
     for (int a=0;a<3;a++){
